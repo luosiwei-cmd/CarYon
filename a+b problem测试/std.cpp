92b6bbdd1106f82577bdb39695f26b4a988307bb
luosw <61175380+luosiwei-cmd@users.noreply.github.com>
2020-08-03 18:08:43 +0800

    Delete Files

--- a+b problem测试/std.cpp
+++
@@ -1,6 +0,0 @@
-#include<bits/stdc++.h>
-int main(){
-	int a,b;
-	std::cin>>a>>b;
-	std::cout<<a+b;
-}
---
+++ example/a+b problem test/std.cpp
@@ -0,0 +1,6 @@
+#include<bits/stdc++.h>
+int main(){
+	int a,b;
+	std::cin>>a>>b;
+	std::cout<<a+b;
+}
