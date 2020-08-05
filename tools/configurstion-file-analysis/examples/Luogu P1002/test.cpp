13ae3a1bb9e4f1cce88747f2435bbf0c6450d091
luosw <61175380+luosiwei-cmd@users.noreply.github.com>
2020-08-05 11:27:52 +0800

    Add some examples

---
+++ tools/configurstion-file-analysis/examples/Luogu P1002/test.cpp
@@ -0,0 +1,22 @@
+#include"caryon.h"
+using namespace std;
+using namespace ca;
+
+int main(){
+    makein(1,10){
+        csh();
+        int m;
+        int n;
+        m=cyrand(1,20);
+        n=cyrand(1,20);
+        inint(n);
+        instring(" ");
+        inint(m);
+        instring(" ");
+        inint(cyrand(0,20));
+        instring(" ");
+        inint(cyrand(0,20));
+    }
+    makeout(1,10);
+    return 0;
+}
---
+++ tools/configurstion-file-analysis/examples/Luogu P1003/test.cpp
@@ -0,0 +1,28 @@
+#include"caryon.h"
+using namespace std;
+using namespace ca;
+
+int main(){
+    makein(1,10){
+        csh();
+        int n;
+        n=1000;
+        int i;
+        int j;
+        inint(n);
+        instring("\n");
+        for(i=1;i<=n;i+=1){
+            for(j=1;j<=4;j+=1){
+                inint(cyrand(0,100));
+                instring(" ");
+            }
+            instring("\n");
+        }
+        for(i=1;i<=2;i+=1){
+            inint(cyrand(1,99));
+            instring(" ");
+        }
+    }
+    makeout(1,10);
+    return 0;
+}
