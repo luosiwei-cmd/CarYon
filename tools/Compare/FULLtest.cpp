109df73691b6e547fc7251cdbaff3acc754afa94
luosw <61175380+luosiwei-cmd@users.noreply.github.com>
2020-08-07 14:47:36 +0800

    Add FULLtest

--- tools/Compare/FULLtest.cpp
+++ tools/Compare/FULLtest.cpp
@@ -1,30 +1,30 @@
 #include<bits/stdc++.h>
+#include<ctime>
 using namespace std;
 #define UKE -1
 #define AC 1
 #define WA 0
 int compareFile(FILE* file_compared, FILE* file_checked){
-    bool diff=0;
-    int N=65536;
-    char b1[N],b2[N];
-    size_t s1,s2;
-    while(1){   
-		if(fgets(b1,N,file_compared)==NULL||fgets(b2,N,file_checked)==NULL){
-			break;
-		}  
-        s1=strlen(b1);
-        s2=strlen(b2);
-        if((s1!=s2&&s1+2!=s2&&s1-2!=s2&&s1+1!=s2&&s1-1!=s2)||memcmp(b1,b2,min(s1,s2)-1)){
-            diff=1;
+    bool diff = 0;
+    int N = 65536;
+    char* b1 = (char*) calloc (1, N+1);
+    char* b2 = (char*) calloc (1, N+1);
+    size_t s1, s2;
+
+    do {
+        s1= fread(b1,1, N, file_compared);
+        s2 = fread(b2,1, N, file_checked);
+        if ((s1!=s2)||memcmp(b1, b2, s1)) {
+            diff = 1;
             break;
         }
-    }
+    }while(!feof(file_compared)||!feof(file_checked));
+
     free(b1);
     free(b2);
-    if(diff)
-		return 0;
-    else 
-		return 1;
+
+    if (diff) return 0;
+    else return 1;
 }
 int main(int argc,char* argv[]){
 	if(argc<=2){
