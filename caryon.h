5d8e59f1e5bd0c64dcf171388799453228bf9192
luosw <61175380+luosiwei-cmd@users.noreply.github.com>
2020-08-23 00:19:50 +0800

    [BUG] Fix.

--- caryon.h
+++ caryon.h
@@ -1,6 +1,91 @@
 #ifndef CRAYON_H
 #define CRAYON_H
-#include <bits/stdc++.h>
+#ifndef _GLIBCXX_NO_ASSERT
+#    include <cassert>
+#endif
+#include <cctype>
+#include <cerrno>
+#include <cfloat>
+#include <ciso646>
+#include <climits>
+#include <clocale>
+#include <cmath>
+#include <csetjmp>
+#include <csignal>
+#include <cstdarg>
+#include <cstddef>
+#include <cstdio>
+#include <cstdlib>
+#include <cstring>
+#include <ctime>
+
+#if __cplusplus >= 201103L
+#    include <ccomplex>
+#    include <cfenv>
+#    include <cinttypes>
+#    include <cstdalign>
+#    include <cstdbool>
+#    include <cstdint>
+#    include <ctgmath>
+#    include <cwchar>
+#    include <cwctype>
+#endif
+
+// C++
+#include <algorithm>
+#include <bitset>
+#include <complex>
+#include <deque>
+#include <exception>
+#include <fstream>
+#include <functional>
+#include <iomanip>
+#include <ios>
+#include <iosfwd>
+#include <iostream>
+#include <istream>
+#include <iterator>
+#include <limits>
+#include <list>
+#include <locale>
+#include <map>
+#include <memory>
+#include <new>
+#include <numeric>
+#include <ostream>
+#include <queue>
+#include <set>
+#include <sstream>
+#include <stack>
+#include <stdexcept>
+#include <streambuf>
+#include <string>
+#include <typeinfo>
+#include <utility>
+#include <valarray>
+#include <vector>
+
+#if __cplusplus >= 201103L
+#    include <array>
+#    include <atomic>
+#    include <chrono>
+#    include <condition_variable>
+#    include <forward_list>
+#    include <future>
+#    include <initializer_list>
+#    include <mutex>
+#    include <random>
+#    include <ratio>
+#    include <regex>
+#    include <scoped_allocator>
+#    include <system_error>
+#    include <thread>
+#    include <tuple>
+#    include <type_traits>
+#    include <typeindex>
+#    include <unordered_map>
+#    include <unordered_set>
+#endif
 #define db double
 #define ll long long
 #define ld long db
@@ -886,7 +971,7 @@ graph< T > connect_graph(int n, int m, T mn, T mx, T (*randfunc)(T, T)) {
     return ret;
 }
 }  // namespace cgraph
-#if __cplusplus >= 201103
+#if __cplusplus >= 201103L
 namespace caryon_cpp11 {
 template < typename T >
 T choice_plus(std::initializer_list< T > a) {
@@ -905,7 +990,7 @@ using namespace inout;
 using namespace cydebug;
 using namespace cconst;
 using namespace cgraph;
-#if __cplusplus >= 201103
+#if __cplusplus >= 201103L
 using namespace caryon_cpp11;
 #endif  //#if __cplusplus>=201103
 }  // namespace ca
